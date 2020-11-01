4783590b8f42f03689f2e589be5978c850f69f50
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-01 20:53:29 +0530

    Added : Ability to connect to payroll service database

--- src/main/java/com/rohan/employeepayrolljdbc/DBDemo.java
+++ src/main/java/com/rohan/employeepayrolljdbc/DBDemo.java
@@ -1,5 +1,37 @@
 package com.rohan.employeepayrolljdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DBDemo {
-	
+	public static void main(String[] args) {
+		String jdbcurl = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "rpatil";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded Successfully");
+		} catch (ClassNotFoundException exception) {
+			throw new IllegalStateException("Cannot find driver in the classpath", exception);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database: " + jdbcurl);
+			connection = DriverManager.getConnection(jdbcurl, userName, password);
+			System.out.println("Connection is successful: " + connection);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("  " + driverClass.getClass().getName());
+		}
+	}
 }
